296f21b882d6c08fac0de7c90d26b8b1a6ba2c8f
Gary Kane <gkane@altoneuroscience.com>
2022-11-17 15:01:00 -0800

    unit tests for continuous and homogenous

---
+++ tests/testthat/test-mmrm_homogeous.R
@@ -0,0 +1,43 @@
+library(MMRM)
+library(testthat)
+
+if (!requireNamespace("lme4")) {
+  stop("lme4 package not installed and is needed to run tests!")
+} else {
+  library(lme4)
+}
+
+###
+
+test_mmrm <- function(d) {
+  expect_no_error(
+    m <- mmrm(chg ~ base + arm + time + base:time + arm:time,
+              time = "time",
+              subjects = "subject",
+              heterogenous=FALSE,
+              data = d
+    )
+  )
+  expect_no_error(
+    m_emm <- mmrm_emmeans(m, group="arm", mode = "kenward", information = "expected")
+  )
+  expect_no_error(
+    m_eff <- mmrm_eff_size(m, m_emm)
+  )
+}
+
+
+
+test_that(
+  "Test homogenous variance model doesn't throw error",
+  {
+    set.seed(3)
+    for (i in 1:10) {
+      n_subs <- sample(10:50, 1)
+      n_timepoints <- sample(2:6, 1)
+      p_missing <- runif(n_timepoints) / 2
+      sim_data <- mmrm_simulate(n_subs, n_timepoints, p_missing = p_missing)
+      test_mmrm(sim_data)
+    }
+  }
+)
